bffb9a0147dc4750410fb82103e6dd0d9f4c7597
pauldambra <paul.dambra@gmail.com>
2018-02-04 11:18:11 +0000

    first test that starts to move to Match hiding scoring from the world

--- src/main/java/com/paul/teventis/match/Match.java
+++ src/main/java/com/paul/teventis/match/Match.java
@@ -2,22 +2,25 @@ package com.paul.teventis.match;
 
 import com.paul.teventis.events.Event;
 import com.paul.teventis.events.EventStore;
+import com.paul.teventis.game.Game;
 
 import java.util.function.Consumer;
 
 public class Match {
 
-    private Consumer<String> subscription;
+    private final Game game;
 
     public Match(EventStore eventStore, String matchId) {
 
         final String stream = streamNameFor(matchId);
         eventStore.readAll(stream).forEach(this::when);
         eventStore.subscribe(stream, this::when);
+
+        this.game = new Game();
     }
 
     private void when(Event event) {
-
+        game.when(event);
     }
 
     public static String streamNameFor(String matchId) {
@@ -25,6 +28,6 @@ public class Match {
     }
 
     public void subscribeToGameScore(Consumer<String> subscription) {
-        this.subscription = subscription;
+        game.subscribeToScore(subscription);
     }
 }
--- src/test/java/com/paul/teventis/PointsAreScoredInAMatch.java
+++ src/test/java/com/paul/teventis/PointsAreScoredInAMatch.java
@@ -1,6 +1,7 @@
 package com.paul.teventis;
 
 import com.paul.teventis.game.PlayerOneScored;
+import com.paul.teventis.game.PlayerTwoScored;
 import com.paul.teventis.match.Match;
 import org.junit.Test;
 
@@ -13,17 +14,17 @@ public class PointsAreScoredInAMatch {
     private String reportedScore = "";
 
     @Test
-    public void andItReportsTheCurrentScore() {
+    public void andItReportsTheCurrentGameScore() {
         String matchId = UUID.randomUUID().toString();
-
         final FakeEventStore inMemoryEventStream = new FakeEventStore();
 
         final Match match = new Match(inMemoryEventStream, matchId);
-
         match.subscribeToGameScore(s -> reportedScore = s);
 
         inMemoryEventStream.write(Match.streamNameFor(matchId), new PlayerOneScored());
-
         assertThat(reportedScore).isEqualTo("15-love");
+
+        inMemoryEventStream.write(Match.streamNameFor(matchId), new PlayerTwoScored());
+        assertThat(reportedScore).isEqualTo("15-all");
     }
 }
